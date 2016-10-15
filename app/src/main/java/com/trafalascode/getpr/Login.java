4a69f15853ca39a17f063d0236a5b94963f4026e
José Alfredo Valles <alfredo.valles390@gmail.com>
2016-10-15 15:21:36 -0400

    Added parse database dependencies.

--- app/src/main/java/com/trafalascode/getpr/Login.java
+++ app/src/main/java/com/trafalascode/getpr/Login.java
@@ -2,6 +2,7 @@ package com.trafalascode.getpr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void CredentialLogin(View view) {
     }
 }
