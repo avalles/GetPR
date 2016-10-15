0c5c201474f4b984ed6aa2ab3cc3bd0c53a8d4f0
Luis <albertorio.10@hotmail.com>
2016-10-15 16:16:08 -0400

    user signed up

--- app/src/main/java/com/trafalascode/getpr/MainActivity.java
+++ app/src/main/java/com/trafalascode/getpr/MainActivity.java
@@ -5,12 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.parse.Parse;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+        .applicationId("7oYbPZwkqpIvKxgrYis6BtBz2cygQRlBTmqK2Ufv")
+        .clientKey("51h9KKW4eciUoZRl09qD7hcilttHkE1UVC0yyeeJ")
+        .server("https://parseapi.back4app.com/")
+        .build());
     }
 
     public void login(View view) {
--- app/src/main/java/com/trafalascode/getpr/SignUp.java
+++ app/src/main/java/com/trafalascode/getpr/SignUp.java
@@ -1,13 +1,57 @@
 package com.trafalascode.getpr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignUp extends AppCompatActivity {
+
+    EditText email, pass;
+    Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        email = (EditText)findViewById(R.id.signUpEmailEditText);
+        pass = (EditText)findViewById(R.id.signUpPassEditText);
+        signUp = (Button)findViewById(R.id.signUpButton);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSignUpPressed();
+            }
+        });
+
+    }
+
+    public void onSignUpPressed(){
+        ParseUser user = new ParseUser();
+        user.setUsername("test");
+        user.setEmail(email.getText().toString());
+        user.setPassword(pass.getText().toString());
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e==null){
+                    Toast.makeText(SignUp.this, "User signed up", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(SignUp.this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
