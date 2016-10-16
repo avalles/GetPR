44bc2e719e4970b7b5c3cdc8b7b9b44e79997680
Luis <albertorio.10@hotmail.com>
2016-10-15 23:01:16 -0400

    map

--- app/src/main/java/com/trafalascode/getpr/EventList.java
+++ app/src/main/java/com/trafalascode/getpr/EventList.java
@@ -2,6 +2,9 @@ package com.trafalascode.getpr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.parse.FindCallback;
@@ -16,6 +19,9 @@ public class EventList extends AppCompatActivity {
 
     ListView eventList;
     EventListAdapter adapter;
+    Button west;
+
+    ImageView mapImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +29,20 @@ public class EventList extends AppCompatActivity {
         setContentView(R.layout.activity_event_list);
 
         eventList = (ListView)findViewById(R.id.eventListListView);
+        west = (Button)findViewById(R.id.button2);
+        mapImage = (ImageView)findViewById(R.id.mapImageView);
 
         adapter = new EventListAdapter(this);
         eventList.setAdapter(adapter);
+
+        west.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("click");
+                mapImage.setImageResource(R.drawable.oeste);
+//                mapImage.setImageDrawable(getResources().getDrawable(R.drawable.oeste));
+            }
+        });
 
         ParseQuery<ParseObject> query = new ParseQuery<>("event");
         query.findInBackground(new FindCallback<ParseObject>() {
