bcff0fad34b0e9393e7b07604bac3e964b963384
Luis <albertorio.10@hotmail.com>
2016-10-15 22:43:20 -0400

    video up

--- app/src/main/java/com/trafalascode/getpr/MainActivity.java
+++ app/src/main/java/com/trafalascode/getpr/MainActivity.java
@@ -1,17 +1,51 @@
 package com.trafalascode.getpr;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.VideoView;
 
 import com.parse.Parse;
 
 public class MainActivity extends AppCompatActivity {
 
+    VideoView videoView;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(videoView != null){
+            String uriPath = "android.resource://"+getPackageName()+"/" + + R.raw.getpr;
+            Uri uri = Uri.parse(uriPath);
+            videoView.setVideoURI(uri);
+            videoView.start();
+
+
+            /**
+             * Set the video loop infinitely
+             */
+            videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.setLooping(true);
+                }
+            });
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
         setContentView(R.layout.activity_main);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
@@ -19,6 +53,20 @@ public class MainActivity extends AppCompatActivity {
         .clientKey("51h9KKW4eciUoZRl09qD7hcilttHkE1UVC0yyeeJ")
         .server("https://parseapi.back4app.com/")
         .build());
+
+        videoView = (VideoView)findViewById(R.id.videoView);
+        String uriPath = "android.resource://"+getPackageName()+"/" + R.raw.getpr;
+        Uri uri = Uri.parse(uriPath);
+        videoView.setVideoURI(uri);
+        videoView.start();
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
+
     }
 
     public void login(View view) {
